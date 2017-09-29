dfd3f8130866889290dfd5fbbae056652a3d4333
Jake 'Sid' Smith <jake@dabapps.com>
2017-09-29 12:45:51 +0100

    Test for uniqueness

--- tests/index.ts
+++ tests/index.ts
@@ -1,20 +1,34 @@
 import { fuuid } from '../src/';
+import { LENGTH } from '../src/constants';
+import { terms } from '../src/terms';
+
+const MATCHES_FUUID = /^[a-z]+(?:-[a-z]+){15}$/;
+const MATCHES_CENSORED_FUUID = /^\*+(?:-\*+){15}$/;
+
+const UUID_CHAR_COUNT = 16;
+const UUID_LENGTH = 32;
+
+function getVariationCount (chars: number, length: number) {
+  return Math.pow(chars, length);
+}
 
 describe('fuuid', () => {
-
-  const MATCHES_FUUID = /^[a-z]+(?:-[a-z]+){31}$/;
-  const MATCHES_CENSORED_FUUID = /^\*+(?:-\*+){31}$/;
 
   it('generates a string', () => {
     expect(typeof fuuid()).toBe('string');
   });
 
-  it('generates a 32 term string with hyphen separators', () => {
+  it('generates a 16 term string with hyphen separators', () => {
     expect(MATCHES_FUUID.test(fuuid())).toBe(true);
   });
 
   it('should optionally censor the fuuid', () => {
     expect(MATCHES_CENSORED_FUUID.test(fuuid(true))).toBe(true);
+  });
+
+  it('should be more unique than a UUID', () => {
+    expect(getVariationCount(terms.length, LENGTH))
+      .toBeGreaterThan(getVariationCount(UUID_CHAR_COUNT, UUID_LENGTH));
   });
 
 });
